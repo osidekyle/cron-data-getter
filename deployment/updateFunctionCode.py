823bd4222266652a61f286ccf57c74d6f7948e24
osidekyle <hawkinskyle67@gmail.com>
2022-09-07 21:56:55 -0700

    added creds, added new function and new deployment steps

--- deployment/updateFunctionCode.py
+++ deployment/updateFunctionCode.py
@@ -5,5 +5,11 @@ client = boto3.client("lambda")
 client.update_function_code(
     FunctionName='cron-job-data-getter',
     S3Bucket='news-artifacts-kvh',
-    S3Key='cron-data-getter/lambda.zip'
+    S3Key='cron-data-getter/data_getter.zip'
+)
+
+client.update_function_code(
+    FunctionName='moveNewsToRDS',
+    S3Bucket='news-artifacts-kvh',
+    S3Key='cron-data-getter/move_rds.zip'
 )
