1785bca9439d1b0a659286992871859b3acf93a4
osidekyle <hawkinskyle67@gmail.com>
2022-09-09 22:56:47 -0700

    debugs

--- src/move_to_rds/move_to_rds.py
+++ src/move_to_rds/move_to_rds.py
@@ -34,8 +34,12 @@ def handler(event, context):
 
     s3 = boto3.client("s3")
 
+    print("Before read")
+
     file_content = s3.get_object(
         Bucket=bucket_name, Key=key_name)["Body"].read()
+
+    print("After read")
 
     df = pd.read_csv(file_content)
 
