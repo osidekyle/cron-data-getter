521771be15c0f8ee9f382a1d3e7b486f94d5cf2b
osidekyle <hawkinskyle67@gmail.com>
2022-09-17 19:54:44 -0700

    append to table instead of replace table

--- src/move_to_rds/move_to_rds.py
+++ src/move_to_rds/move_to_rds.py
@@ -42,7 +42,7 @@ def handler(event, context):
 
     with conn.cursor() as cur:
         cur.execute("create table IF NOT EXISTS NewsStories( title varchar(255) NOT NULL, description varchar(255) NOT NULL, author varchar(255) NOT NULL, date varchar(255) NOT NULL, link varchar(255) NOT NULL, source varchar(255) NOT NULL, created_date DATE NOT NULL, PRIMARY KEY (Link))")
-        df.to_sql(con=engine, name='NewsStories', if_exists='replace')
+        df.to_sql(con=engine, name='NewsStories', if_exists='append')
         conn.commit()
         cur.execute("select * from NewsStories")
         for row in cur:
